6bea48f0701c258bf89c74babae859713feaa4c5
myoung128 <michaels.young@libertymutual>
2017-10-20 16:17:00 -0400

    Updated event controller during troubleshooting of post
    updated potential syntax issues for update, during
    post trouble shooting.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -30,7 +30,7 @@ class EventsController < ProtectedController
   # PATCH/PUT /events/1
   def update
     # if @event.update(event_params)
-    if @event.current_user.events.update(event_params)
+    if @event.update(event_params)
       render json: @event
     else
       render json: @event.errors, status: :unprocessable_entity
@@ -43,14 +43,17 @@ class EventsController < ProtectedController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_event
-      @event = Event.find(params[:id])
-      # @event = current_user.events.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def event_params
-      params.require(:event).permit(:name, :description, :event_type, :event_category, :time_required, :details, :completed, :location, :user_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_event
+    @event = current_user.events.find(params[:id])
+    # @event = current_user.events.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def event_params
+    params.require(:event)
+          .permit(:name, :description, :event_type, :event_category,
+                  :time_required, :details, :completed, :location)
+  end
 end
